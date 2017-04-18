50cef542f78e0b9b8fb867027882e01caf5fcabe
KiranTurimella <KiranTurimella@ravis-mbp>
2017-04-17 20:58:47 -0500

    My First Commit

---
+++ MyFirstJavaProject/src/com/myorg/training/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.myorg.training;
+
+/**
+ * @author KiranTurimella
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
